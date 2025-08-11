569d0dcedd96164daa92202a5ffc5184dac05893
Naveen Singh <143250029+Naveen322singh@users.noreply.github.com>
2025-08-12 00:41:44 +0530

    Time: 0 ms (100%), Space: 42.1 MB (47.07%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void sortColors(int[] arr) {
+        int n=arr.length;
+        int count_0=0;
+        int count_1=0;
+
+        for(int i=0;i<n;i++){
+            if(arr[i]==0){
+                count_0++;
+            }
+            else if(arr[i]==1){
+                count_1++;
+            }
+
+        }
+        for(int i=0;i<count_0;i++){
+            arr[i]=0;
+        }
+        for(int i=count_0;i<count_0+count_1;i++){
+            arr[i]=1;
+        }
+        for(int i=count_0+count_1;i<n;i++){
+            arr[i]=2;
+        }
+    }
+}
