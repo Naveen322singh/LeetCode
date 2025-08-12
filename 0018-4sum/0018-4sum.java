dfb15f50f9db69faea0127dc89ac47668a625d2c
Naveen Singh <143250029+Naveen322singh@users.noreply.github.com>
2025-08-13 00:05:45 +0530

    Time: 479 ms (5%), Space: 45.5 MB (10.14%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,31 +1,28 @@
 class Solution {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-        int n = nums.length;
-        Set<List<Integer>> st = new HashSet<>();
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                Set<Long> hashset = new HashSet<>();
-                for (int k = j + 1; k < n; k++) {
-                    long sum = nums[i] + nums[j];
-                    sum+=nums[k];
-                    long fourth = target - sum;
-                    if (hashset.contains(fourth)) {
-                        List<Integer> temp= new ArrayList<>();
-                        temp.add(nums[i]);
-                        temp.add(nums[j]);
-                        temp.add(nums[k]);
+    public List<List<Integer>> fourSum(int[] arr, int target) {
+        int n=arr.length;
+        Set<List<Integer>> st=new HashSet<>();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                Set<Long> hashset=new HashSet<>();
+                for(int k=j+1;k<n;k++){
+                    long sum=arr[i]+arr[j];
+                    sum+=arr[k];
+                    long fourth=target-sum;
+                    if(hashset.contains(fourth)){
+                        List<Integer> temp=new ArrayList<>();
+                        temp.add(arr[i]);
+                        temp.add(arr[j]);
+                        temp.add(arr[k]);
                         temp.add((int)fourth);
                         temp.sort(null);
                         st.add(temp);
                     }
-                    // put the kth element into the hashset:
-                    hashset.add((long) nums[k]);
+                    hashset.add((long)arr[k]);
                 }
             }
         }
-        List<List<Integer>> ans = new ArrayList<>(st);
+        List<List<Integer>> ans=new ArrayList<>(st);
         return ans;
-        
     }
 }
