13e3dcc61e169de7c6d6a77b6ef46f0c579063e6
Naveen Singh <143250029+Naveen322singh@users.noreply.github.com>
2025-08-13 22:24:51 +0530

    Time: 17 ms (44.39%), Space: 46.8 MB (13.43%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -4,16 +4,15 @@ class Solution {
         int nge[] = new int[n];
         Stack < Integer > st = new Stack < > ();
         for (int i = 2 * n - 1; i >= 0; i--) {
-            while (st.isEmpty() == false && st.peek() <= nums[i % n]) {
+            while (!st.isEmpty()  && st.peek() <= nums[i % n]) {
                 st.pop();
             }
-
             if (i < n) {
-                if (st.isEmpty() == false) {
-                    nge[i] = st.peek();
+                if (st.isEmpty()) {
+                    nge[i] = -1;
                 }
                 else {
-                    nge[i] = -1;
+                    nge[i] = st.peek();
                 }
             }
             st.push(nums[i % n]);
