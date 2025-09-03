51bdc1dca7e1bafdec34cac30ed04b3649856a12
Naveen Singh <143250029+Naveen322singh@users.noreply.github.com>
2025-09-03 16:33:08 +0530

    Time: 25 ms (85.7%), Space: 45.3 MB (88.8%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.java
+++ 0876-hand-of-straights/0876-hand-of-straights.java
@@ -1,37 +1,25 @@
-import java.util.*;
-
-public class Solution {
+class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
-        int n = hand.length;
-        if (n % groupSize != 0) {
-            return false;
-        }
-        
         Map<Integer, Integer> countMap = new HashMap<>();
+        for (int card : hand) {
+            countMap.put(card, countMap.getOrDefault(card, 0) + 1);
+        }
+        
+        Arrays.sort(hand);
+        
         for (int i = 0; i < hand.length; i++) {
-            countMap.put(hand[i], countMap.getOrDefault(hand[i], 0) + 1);
-        }
-        
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        Set<Integer> keys = countMap.keySet();
-        for (Iterator<Integer> it = keys.iterator(); it.hasNext(); ) {
-            int key = it.next();
-            minHeap.offer(key);
-        }
-        
-        while (!minHeap.isEmpty()) {
-            int first = minHeap.peek();
-            for (int i = first; i < first + groupSize; i++) {
-                if (!countMap.containsKey(i) || countMap.get(i) == 0) {
+            if (countMap.get(hand[i]) == 0) {
+                continue;
+            }
+            
+            for (int j = 0; j < groupSize; j++) {
+                int currCard = hand[i] + j;
+                
+                if (countMap.getOrDefault(currCard, 0) == 0) {
                     return false;
                 }
-                countMap.put(i, countMap.get(i) - 1);
-                if (countMap.get(i) == 0) {
-                    if (minHeap.peek() != i) {
-                        return false;
-                    }
-                    minHeap.poll();
-                }
+                
+                countMap.put(currCard, countMap.get(currCard) - 1);
             }
         }
         
