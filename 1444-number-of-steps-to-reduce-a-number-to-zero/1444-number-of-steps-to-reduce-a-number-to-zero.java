0268811294164d43cc7ebd322344a326ba3a1225
Naveen Singh <143250029+Naveen322singh@users.noreply.github.com>
2025-09-09 18:29:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static int number_of_steps(int num){
+        return helper(num,0);
+    }
+    private static int helper(int num,int steps){
+        if(num==0){
+            return steps;
+        }
+        if(num%2==0){
+            return helper(num/2,steps+1);
+        }
+        return helper(num-1,steps+1);
+    }
+}
