a93ef91ae6846ad3465ffe05d5c4b10c7fa46682
Naveen Singh <143250029+Naveen322singh@users.noreply.github.com>
2025-09-16 00:19:26 +0530

    Time: 2 ms (79.41%), Space: 46.2 MB (54.65%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,12 +1,12 @@
 class Solution {
     public void moveZeroes(int[] arr) {
         int n=arr.length;
-        int index=0;
-        int [] temp=new int [n];
+        int count=0;
+        int [] temp=new int[n];
+        int j=0;
         for(int i=0;i<n;i++){
             if(arr[i]!=0){
-                temp[index]=arr[i];
-                index++;
+                temp[j++]=arr[i];
             }
         }
         for(int i=0;i<temp.length;i++){
@@ -14,6 +14,7 @@ class Solution {
         }
         for(int i=temp.length;i<n;i++){
             arr[i]=0;
-        } 
+        }
+
     }
 }
