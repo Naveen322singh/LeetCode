c46e4ad1a2de1dfe0946b18d7d71c91546309d47
Naveen Singh <143250029+Naveen322singh@users.noreply.github.com>
2025-09-16 00:36:15 +0530

    Time: 1 ms (33.21%), Space: 45 MB (94.19%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -17,9 +17,8 @@ class Solution {
             }
         }
         return n;
-        
     }
-    public static void swap(int []arr,int first,int second){
+    public void swap(int[]arr,int first,int second){
         int temp=arr[first];
         arr[first]=arr[second];
         arr[second]=temp;
