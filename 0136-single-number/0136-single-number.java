b26722a6d10a3ac43d7fce72e49e7cb4a4a368b4
Naveen Singh <143250029+Naveen322singh@users.noreply.github.com>
2025-09-16 00:40:02 +0530

    Time: 1 ms (99.85%), Space: 46.5 MB (26.58%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -2,7 +2,7 @@ class Solution {
     public int singleNumber(int[] arr) {
         int xor=0;
         for(int i=0;i<arr.length;i++){
-            xor=xor^arr[i];
+            xor=arr[i]^xor;
         }
         return xor;
     }
