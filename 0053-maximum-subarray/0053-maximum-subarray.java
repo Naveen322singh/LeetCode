17171457f31a6256c295cd209db9ec02fb858eec
Naveen Singh <143250029+Naveen322singh@users.noreply.github.com>
2026-04-28 01:00:40 +0530

    Time: 1 ms (99.97%), Space: 77.2 MB (49.47%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -4,11 +4,12 @@ class Solution {
         int maxi=Integer.MIN_VALUE;
         for(int i=0;i<nums.length;i++){
             sum+=nums[i];
-            maxi=Math.max(maxi,sum);
+            if(sum>maxi){
+                maxi=sum;
+            }
             if(sum<0){
                 sum=0;
             }
-
         }
         return maxi;
         
