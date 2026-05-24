6ba2ea0f9469a72499750ce1075aa78377ed0f4e
Naveen Singh <143250029+Naveen322singh@users.noreply.github.com>
2026-05-24 20:29:10 +0530

    Time: 12 ms (29.86%), Space: 50.2 MB (55.09%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -2,24 +2,26 @@ class Solution {
     public int shipWithinDays(int[] weights, int days) {
         int n=weights.length;
         int max_sum=0;
-        int maxi=Integer.MIN_VALUE;
+        int mini=Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
             max_sum+=weights[i];
-            maxi=Math.max(maxi,weights[i]);
+            mini=Math.max(mini,weights[i]);
         }
-        int low=maxi;
+        int ans=-1;
+        int low=mini;
         int high=max_sum;
         while(low<=high){
             int mid=(low+high)/2;
             int calculate_days=days_needed(weights,mid);
             if(calculate_days<=days){
+                ans=mid;
                 high=mid-1;
             }
             else{
                 low=mid+1;
             }
         }
-        return low;
+        return ans;
     }
     public int days_needed(int []weights,int capacity){
         int load=0;
